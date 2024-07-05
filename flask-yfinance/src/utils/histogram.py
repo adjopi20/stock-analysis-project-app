065fe87233fce44d67c4c1cc0c5748166bb245c2
JOPI DOLOKSARIBU <jopi.adrianto@gmail.com>
2024-07-05 21:36:02 +0700

    feat: histogram group analysis -> done

--- flask-yfinance/src/utils/histogram.py
+++ flask-yfinance/src/utils/histogram.py
@@ -6,21 +6,17 @@ import numpy as np
 from astropy.stats import knuth_bin_width
 from services.histogram_sector_service import *
 
-def histogram_tool (sector, category, listBoard=None, industry=None, marketCap=None, recKey=None, recMean=None):
-    dataset = get_stock_info_for_histogram(sector, category, listBoard, industry, marketCap, recKey, recMean)
+def histogram_tool (dataset, category: str):
     values = []
     try: 
         for data in dataset :
             if pd.notna(data[category]):     
-
-            # data ['sector'] == sector and category in data and 
-               
                 values.append(data[category])
             else:
                 pass
         width, bin_edges = knuth_bin_width(values, return_bins=True) #compute the how many bins to use
         freq, bin_edges = np.histogram(values, bins=bin_edges) #determine variable for histogram
-        bin_edges = [round(x, 2) for x in bin_edges] #bulatkan ke 2 angka di blkg koma, disini ndarray sudah diubah jadi list
+        bin_edges = [round(x, 3) for x in bin_edges] #bulatkan ke 2 angka di blkg koma, disini ndarray sudah diubah jadi list
 
         print(f"freq: {len(freq)}, bin_edges2: {len(bin_edges)}") 
         return freq, bin_edges
--- flask-yfinance/src/utils/mean.py
+++ flask-yfinance/src/utils/mean.py
@@ -6,17 +6,14 @@ import scipy.stats as stats
 import astropy.stats as astats
 
 
-def trimmed_mean():
-    tes = fetched_info_with_cache()
+def trimmed_mean(dataset, category: str):
 
-    # tes_z_score = stats.zscore(tes)
-
-    
     baskom = []
-    for item in tes:
-        if 'returnOnEquity' not in item:
+    for item in dataset:
+        if category not in item:
            continue
-        numbers = item.get('returnOnEquity', np.nan)
+        
+        numbers = item.get(category, np.nan)
         
         if np.isnan(numbers) or numbers is None:
             continue 
@@ -26,23 +23,14 @@ def trimmed_mean():
     if baskom:
         zscore = stats.zscore(baskom)
     
-    print(f"z score: {len(zscore)}")
-
-    # for item1,item2 in zip(baskom, zscore):
-    #     if baskom[item1] == zscore[item2]:
     filtered = filter(lambda x: x<=3 and x>=-3, zscore)
     filtered = list(filtered)
-    #         return baskom if baskom[item1] == 
-    print(f"len filtered zscore: {len(filtered)}\nfiltered zscore: {filtered}")
-
+    
     threshold = 3
     mask = np.abs(zscore) < threshold
     baskom = np.array(baskom)
     cleaned = baskom[mask]
 
-    print(f"data cleaned length: {len(cleaned)}\ndata cleaned value: {cleaned}")
+    x = round(np.mean(cleaned), 4)
 
-    x = round(np.mean(cleaned), 2)
-    print(f"rounded: {x}")
-
-trimmed_mean()
+    return x
