0ad27e659ffd1a38c28897ad4f8e2b8e6e98bcfd
JOPI DOLOKSARIBU <jopi.adrianto@gmail.com>
2024-08-17 00:19:19 +0700

    top gainers -> on progress

--- frontend/src/app/features/components/overview/overview.component.ts
+++ frontend/src/app/features/components/overview/overview.component.ts
@@ -21,6 +21,8 @@ import { firstValueFrom } from 'rxjs';
 export class OverviewComponent {
   stocks: any[] = [];
   topGainers: any[] = [];
+  weeklyGainers: any[] = [];
+  monthlyGainers: any[] = [];
   topLosers: any[] = [];
   topDividends: any[] = [];
   topEarnings: any[] = [];
@@ -56,57 +58,74 @@ export class OverviewComponent {
     }
   }
 
-
   async getHistoricalPrice() {
     try {
-      const data = await firstValueFrom(this.apiService.getHistoricalPrice('1mo'));
-      this.stocks = data["data"];
+      const data = await firstValueFrom(
+        this.apiService.getHistoricalPrice('1mo')
+      );
+      this.stocks = data;
       console.log('this.stocks', this.stocks);
-  
-      for (let item of this.stocks) {
-        if (item.metadata.symbol === 'AALI.JK') {
-          const close = item.history.Close;
-          const dates = Object.keys(close);
-          const latestDate = dates[dates.length - 1]; // Get the latest date
-          const latestPrice = close[latestDate];
-          this.latestPrice = {
-            latestPrice: latestPrice,
-          };
-          console.log('this.latestPrice', this.latestPrice);
-        }
-      }
+      this.topGainerss();
     } catch (error: any) {
       console.log(error);
     } finally {
       console.log('complete');
     }
   }
-  
-  
-
 
   // this.stocks.sort((a, b) => b.marketCap - a.marketCap);
-      // this.topDividends = this.stocks
-      //   .filter((item) => item.dividendRate)
-      //   .sort((a, b) => b.dividendRate - a.dividendRate)
-      //   .slice(0, 10);
-      // this.topMarketCaps = this.stocks
-      //   .filter((item) => item.marketCap)
-      //   .sort((a, b) => b.marketCap - a.marketCap)
-      //   .slice(0, 10);
+  // this.topDividends = this.stocks
+  //   .filter((item) => item.dividendRate)
+  //   .sort((a, b) => b.dividendRate - a.dividendRate)
+  //   .slice(0, 10);
+  // this.topMarketCaps = this.stocks
+  //   .filter((item) => item.marketCap)
+  //   .sort((a, b) => b.marketCap - a.marketCap)
+  //   .slice(0, 10);
 
+  topGainerss() {
+    for (let item of this.stocks) {
+      // if (item.metadata.symbol === 'AALI.JK') {
+      const close = item.history.Close;
+      const dates = Object.keys(close);
+      const latest = dates[dates.length - 1];
+      const latestPrice = close[latest];
+      const start = dates[dates.length - 3];
+      const startPrice = close[start];
+      const end = dates[dates.length - 2];
+      const endPrice = close[end];
 
-  topGainerss(period: string) {
-    // const today: Date = new Date();
-    // const oneDay: number = 24*60*60*1000;
-    // const startDate= new Date(today.getTime()-oneDay*2);
-    // const endDate = new Date(today.getTime()-oneDay);
-    // const start = this.datePipe.transform(startDate, 'yyyy-MM-dd');
-    // const end = this.datePipe.transform(endDate, 'yyyy-MM-dd');
-    const close: any[] = [];
+      const percentChange = (
+        ((endPrice - startPrice) / startPrice) *
+        100
+      ).toFixed(2);
+
+      const tes = {
+        symbol: item.metadata.symbol,
+        price: latestPrice,
+        percentChange: percentChange,
+      };
+      this.topGainers.push(tes);
+    }
+
+    this.topGainers.sort((a, b) => b.percentChange - a.percentChange)
+    this.topGainers = this.topGainers.slice(0, 10);
+    console.log("this.topGainers",this.topGainers);
+    
+    // this.latestPrice = {
+    //   latestPrice: latestPrice,
+    // };
+    // }
   }
 
-  weeklyGainers(symbol: string, period: string, start: string, end: string) {}
+  // const today: Date = new Date();
+  // const oneDay: number = 24*60*60*1000;
+  // const startDate= new Date(today.getTime()-oneDay*2);
+  // const endDate = new Date(today.getTime()-oneDay);
+  // const start = this.datePipe.transform(startDate, 'yyyy-MM-dd');
+  // const end = this.datePipe.transform(endDate, 'yyyy-MM-dd');
+
+  WeeklyGainers(symbol: string, period: string, start: string, end: string) {}
 
   MonthlyGainers(symbol: string, period: string, start: string, end: string) {}
 }
